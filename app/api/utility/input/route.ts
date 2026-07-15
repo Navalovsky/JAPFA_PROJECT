92adf65b8213c0a752f94fc2b00940648916bbc8
Navalovsky <navalovsky_allenzo_33rpl@student.smktelkom-mlg.sch.id>
2026-07-15 09:44:40 +0700

    route.ts

--- app/api/utility/input/route.ts
+++ app/api/utility/input/route.ts
@@ -6,13 +6,14 @@ const prisma = new PrismaClient();
 export async function POST(request: Request) {
   try {
     const body = await request.json();
-    const { type, data } = body; // type bisa 'wtp' atau 'wwtp'
+    const { type, data, uploaded_by } = body; // type bisa 'wtp' atau 'wwtp'
 
     if (type === 'wtp') {
       const newWtp = await prisma.wtp_logs.create({
         data: {
           debit_inlet: parseFloat(data.debit_inlet),
           debit_outlet: parseFloat(data.debit_outlet),
+          uploaded_by: uploaded_by || null,
         }
       });
       return NextResponse.json({ success: true, data: newWtp });
@@ -25,6 +26,7 @@ export async function POST(request: Request) {
           debit_outlet: parseFloat(data.debit_outlet),
           nh3_n: parseFloat(data.nh3_n),
           ph: parseFloat(data.ph),
+          uploaded_by: uploaded_by || null,
         }
       });
       return NextResponse.json({ success: true, data: newWwtp });
